5fc50e2240e5225fed2f46cba456fa25fcb6d3a4
alejandromateo186 <alejandromateo186@gmail.com>
2026-01-14 16:47:54 +0000

    aux

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,22 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+const config = getDefaultConfig(__dirname);
+
+const { resolver } = config;
+
+config.resolver.sourceExts = [
+  ...resolver.sourceExts, 
+  'cjs', 
+  'mjs'
+];
+
+config.resolver.assetExts = [
+  ...resolver.assetExts, 
+  'glb', 
+  'gltf', 
+  'png', 
+  'jpg'
+];
+
+module.exports = withNativeWind(config, { input: './global.css' });
